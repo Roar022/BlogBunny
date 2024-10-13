b4b192f545bee23f8798af13b6fce7383e7ed0d0
Roar022 <krishgarg004@gmail.com>
2024-10-13 17:53:06 +0530

    bug fixes

--- Backend/controller/blogController.ts
+++ Backend/controller/blogController.ts
@@ -2,19 +2,26 @@ const asyncHandler = require("express-async-handler")
 import {PrismaClient, User} from "@prisma/client"
 import {  Request, Response} from "express";
 
-// Extend the Request interface to include a user property
+// Extend the Request interface to include "USER" property
 interface AuthenticatedRequest extends Request {
     user?: any;
 }
 
 export const createBlog = asyncHandler(async(req:AuthenticatedRequest, res:Response)=>{
+    
+    // Get the user from the request object
     const user = req.user;
-    const body = req.body;
-    const {title, label, description}=body;
+
+    // Get the blog from the request body
+    const blog = req.body;
+
+    const {title, label, description}=blog;
+
     if(!title || !label || !description){
         throw new Error("All fields are required.");
     }
-    const prisma = new PrismaClient();
+    const prisma = new PrismaClient(); 
+    
     const newBlog = await prisma.blog.create({
         data: {
           title,
@@ -22,12 +29,24 @@ export const createBlog = asyncHandler(async(req:AuthenticatedRequest, res:Respo
           Description: description,
           userId: user.id, // Assuming userId corresponds to an existing User in your database
         },
-      });
-      await prisma.user.update({
-        where: { id: user.id },
-        data: { blogs: { connect: { id: newBlog.id } } },
-      });
+    });
+    // updates blog in user
+    const updatedUser = await prisma.user.update({
+      where: { id: user.id },
+      data: { blogs: { connect: { id: newBlog.id } } },
+      // include: { blogs: true , Comments: true}, // if we wanted to include the blogs and comments in the response
+    });
+    // console.log(updatedUser);
+
+    // const userWithBlogs = await prisma.user.findUnique({
+    //   where: {
+    //     id: user.id, // or use email, username, etc.
+    //   },
+    //   include: {
+    //     blogs: true, // Include the related blogs
+    //   },
+    // });
+    // console.log(userWithBlogs);
     
-    // console.log(req.user);
     return res.status(200).json(newBlog)
 })
