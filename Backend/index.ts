0d564029b8d2f6c9b80f53fbaf28eedfb20d0992
Roar022 <krishgarg004@gmail.com>
2024-10-13 14:40:28 +0530

    added comments

--- Backend/index.ts
+++ Backend/index.ts
@@ -2,14 +2,20 @@
 import express, { Request, Response } from "express";
 import userRoute from "./routes/userRoute"
 import blogRoute from "./routes/blogRoute"
+
 const cors = require("cors")
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv").config();
-
 const app = express();
+
+// 
 app.use(cookieParser());
+
+// Enables parsing of JSON request bodies
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+
+// Parse incoming requests data
+app.use(express.urlencoded({ extended: true }))
 
 const PORT =  5001;
 app.get("/", (req:any,res:any)=>{
