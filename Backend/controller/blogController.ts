6864bbc1b02e52a4832d46356739e0b65c18c6c4
Dheeraj Pareek <dheerajpareek@Dheerajs-MacBook-Air.local>
2024-10-13 19:04:39 +0530

    updated blog controller

--- Backend/controller/blogController.ts
+++ Backend/controller/blogController.ts
@@ -149,3 +149,131 @@ export const likeBlog = asyncHandler(async(req:AuthenticatedRequest, res:Respons
     await prisma.$disconnect();
   }
 })
+
+export const dislikeBlog = asyncHandler(async(req:AuthenticatedRequest, res:Response)=>{
+  const user = req.user;
+  const body = req.body;
+  const { blogId } = body;
+
+  if (!blogId) {
+    throw new Error("Blog ID is required.");
+  }
+
+  const prisma = new PrismaClient();
+  try{
+
+    const existingBlog = await prisma.blog.findFirst({
+      where: {
+        id: blogId,
+      },
+    });
+  
+    if (!existingBlog) {
+      throw new Error("Blog not found.");
+    }
+  
+    // Check if the user has liked the blog
+    const existingLike = await prisma.likedBlog.findFirst({
+      where: {
+        userId: user.id,
+        blogId: blogId,
+      },
+    });
+  
+    if (!existingLike) {
+      throw new Error("You haven't liked this blog.");
+    }
+  
+    // Remove the liked blog entry
+    const decrement = await prisma.blog.update({
+      where: {
+        id: blogId
+      },
+      data: {
+        likes: {
+          decrement: 1, // Increment the 'likes' count by 1
+        },
+      },
+    })
+    await prisma.likedBlog.delete({
+      where: {
+        id: existingLike.id,
+      },
+    });
+  
+    return res.status(200).json({ message: "Like removed successfully." });
+  }
+  finally{
+  await prisma.$disconnect();
+
+  }
+  // Check if the blog exists
+
+})
+
+export const getAllBlogs = asyncHandler(async (req: Request, res: Response) => {
+  const prisma = new PrismaClient();
+  try{
+
+    const blogs = await prisma.blog.findMany({
+      include: {
+        user: true, // Include the user relation
+      },
+    });
+    // const username =
+  
+    return res.status(200).json({blogs, message:"success"});
+  }
+  finally{
+  await prisma.$disconnect();
+
+  }
+});
+
+export const getBlogById = asyncHandler(async (req: Request, res: Response) => {
+  const blogId = req.params.blogId;
+  
+  const prisma = new PrismaClient();
+
+  try {
+    const blog = await prisma.blog.findUnique({
+      where: {
+        id: blogId,
+      },
+    });
+  
+    if (!blog) {
+      throw new Error("Blog not found.");
+    }
+  
+    return res.status(200).json(blog);
+    
+  } finally{
+  await prisma.$disconnect();
+
+  }
+});
+
+export const getUserBlog = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+  const userId = req.user.id;
+  
+  const prisma = new PrismaClient();
+  try{
+  const blog = await prisma.blog.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+  if (!blog) {
+    throw new Error("Blog not found.");
+  }
+
+  return res.status(200).json({blog, message:"success"});
+    
+  }
+  finally{
+  // console.log("inside finaly statement ")
+    await prisma.$disconnect();
+
+  }
+});
