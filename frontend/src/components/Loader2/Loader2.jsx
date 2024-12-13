446047f20dc4a3db63bb2e972a4c922590be3d1a
Dheeraj Pareek <pareekdheeraj17@gmail.com>
2024-12-13 19:25:25 +0530

    added loader2

---
+++ frontend/src/components/Loader2/Loader2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Loader2.css'
+const Loader2 = () => {
+  return (
+    <div id="outer" className='mx-auto'>
+        <div class="c1"></div>
+        <div class="c2"></div>
+        <div class="c3"></div>
+        <div class="c4"></div>
+    </div>
+  )
+}
+
+export default Loader2
